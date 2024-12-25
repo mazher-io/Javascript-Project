fdfc6ec0ed43e3f8b7a20220ea7d6efd4c29c10f
MH Code <mazheriftikhar1@gmail.com>
2024-12-25 15:33:09 +0500

    25-Dec changes

--- Phase1/Day2.js
+++ Phase1/Day2.js
@@ -12,3 +12,15 @@ const user = {
   },
 };
 console.log(user.greet()); // Output: Hello, Mazher
+
+
+  // Example 2 
+// const information = {
+//   name: "Mazher",
+//   age:  25,
+//   greet(){
+//     return `Hello, ${this.name} , I am ${this.age} years old`;
+//   }
+
+// };
+// console.log(information.greet()); // Output: Hello, Mazher , I am 25 years
